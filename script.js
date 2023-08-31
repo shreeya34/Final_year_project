2505c70a8360a8c68726051a93e2972319077914
shreeya <1shreeya2@gmail.com>
2023-08-31 18:40:12 +0545

    working

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+
 btnPopup.addEventListener('click',()=>{
     wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+
 });
